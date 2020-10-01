72b2aade0697d72faa2ae0c45728f2fee02c3040
tomaszjanrys <tomaszjanrys@gmail.com>
2020-10-01 13:43:56 +0200

    add ejs and create server express

--- rg.js
+++ rg.js
@@ -1,3 +1,17 @@
-console.log(process.argv.forEach(x=>{
-    console.log(x)
-}))
+const express = require('express')
+const app = express()
+
+app.use(express.static('public'));
+app.use('/css',express.static(__dirname+'/css'))
+app.set('view engine', 'ejs');
+app.set('views','./views')
+app.get('/',(req,res)=>{
+    res.render('index')
+})
+app.listen(2000,(err)=>{
+    try{
+        console.info('server running')
+    } catch {
+        console.error(err)
+    }
+})
