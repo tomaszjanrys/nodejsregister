9516899dadcef1f1739119d6d8b7af17dab29912
tomaszjanrys <tomaszjanrys@gmail.com>
2020-10-01 14:32:40 +0200

    ok

--- rg.js
+++ rg.js
@@ -8,6 +8,9 @@ app.set('views','./views')
 app.get('/',(req,res)=>{
     res.render('index')
 })
+app.get('/register', (req,res)=>{
+    res.render('register')
+})
 app.listen(2000,(err)=>{
     try{
         console.info('server running')
